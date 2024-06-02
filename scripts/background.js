ab255964270e8ee76502c381df2bc6c732442f9d
daytona65 <niclim65@gmail.com>
2024-06-02 12:16:34 +0800

    Base for future models

--- scripts/background.js
+++ scripts/background.js
@@ -2,12 +2,3 @@ chrome.runtime.onInstalled.addListener(() => {
     console.log('Extension installed.');
 });
 
-// Called when the user clicks on the browser action(tab)
-// chrome.browserAction.onClicked.addListener(function(tab) {
-//     alert("test");
-// });
-
-let canvas = new OffscreenCanvas(300, 200);
-Promise.all([
-    faceapi
-])
